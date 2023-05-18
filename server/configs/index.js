03fcff53d51a6dff8334f6b4bcf72fabb4d78aa1
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-05-18 14:33:47 +0300

    add io

--- server/configs/index.js
+++ server/configs/index.js
@@ -10,5 +10,9 @@ module.exports = {
       port: 27017,
       dbName: 'fd_chat'
     }
+  },
+  SOCET_EVENTS: {
+    NEW_MESSAGE: 'NEW_MESSAGE',
+    NEW_MESSAGE_ERROR: 'NEW_MESSAGE_ERROR'
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,31 @@
 const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./app');
+const { Message } = require('./models');
+const { SOCET_EVENTS } = require('./configs');
 
 const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, { transports: ['websocket'] });
+
+io.on('conection', (socket) => {
+  console.log('conection socket');
+  socket.on('newMessage', async (message) => {
+    console.log('message ===>>', message);
+    try {
+      const savedMassage = await Message.create(message);
+      if (!savedMassage) {
+        throw new Error('Bad message');
+      }
+      io.emit(SOCET_EVENTS.NEW_MESSAGE, savedMassage);
+    } catch (error) {
+      socket.emit(SOCET_EVENTS.NEW_MESSAGE_ERROR, error);
+    }
+  });
+  socket.on('newMessage', (reason) => {
+    console.log('message ===>>', reason);
+  });
+})
 
 const port = process.env.PORT || 3000
 httpServer.listen(port, () => console.log('server started at port = ' + port));
